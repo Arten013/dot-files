0024f9a4119edab90daf5a9bd5a0f1119482698b
Arten013 <fukkn_kaz@yahoo.co.jp>
2018-07-14 11:06:32 +0900

    auto install nvim (without debuv)

--- etc/init/nvim.sh
+++ etc/init/nvim.sh
@@ -2,6 +2,15 @@
 
 echo "setup nvim "$DOTFILES_DIR"./.vim"
 
+if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    if [ !"$(which apt)" == "" ]; then
+        apt install software-properties-common
+
+        add-apt-repository ppa:neovim-ppa/unstable
+        apt update
+        apt install neovim
+    fi
+fi
 rm -rf $HOME'/.config/nvim'
 mkdir -p $HOME'/.config/nvim'
 ln -sfnv $DOTFILES_DIR'/.vimrc' $HOME'/.config/nvim/init.vim'
